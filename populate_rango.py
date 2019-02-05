2cd9c68ff1d201b3c1afe6ed3c843cb388e8e2e8
Catriona Murphy <2312695m@student.gla.ac.uk>
2019-02-05 12:44:10 +0000

    error corrections

--- populate_rango.py
+++ populate_rango.py
@@ -28,10 +28,10 @@ def populate():
 		"url":"http://flask.pocoo.org", "views":300} ]
 	cats =  {"Python": {"pages": python_pages, "views":128, "likes":64}, 
 		"Django": {"pages": django_pages, "views":64,"likes":32},
-		"Other Frameworks": {"pages": other_pages} }
+		"Other Frameworks": {"pages": other_pages, "views":32, "likes":16} }
 	
 	for cat, cat_data in cats.items():
-		c = add_cat(cat)
+		c = add_cat(cat, cat_data["views"], cat_data["likes"])
 		for p in cat_data["pages"]:
 			add_page(c, p["title"], p["url"], p["views"])
 			
@@ -42,15 +42,15 @@ def populate():
 			
 			
 			
-def add_page(cat, title, url, views):
+def add_page(cat, title, url, views=0):
 	p = Page.objects.get_or_create(category=cat, title=title)[0]
 	p.url = url
 	p.views = views
 	p.save()
 	return p
 	
-def add_cat(name):
-	c =  Category.objects.get_or_create(name=name)[0]
+def add_cat(name,views,likes):
+	c =  Category.objects.get_or_create(name=name, views = views, likes=likes)[0]
 	c.save()
 	return c
 
